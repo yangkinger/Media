3e31ca4f07563ec2ae32050b3421b5118689244d
yangkinger <297323437@qq.com>
2025-01-12 11:33:05 +0800

    Update function.php

--- action/function.php
+++ action/function.php
@@ -1,37 +1,31 @@
-<?php //00363
-// IONCUBE ENCODER 14.0 EVALUATION
-// THIS LICENSE MESSAGE IS ONLY ADDED BY THE EVALUATION ENCODER AND
-// IS NOT PRESENT IN PRODUCTION ENCODED FILES
+<?php
 
-if(extension_loaded('ionCube Loader')){die('The file '.__FILE__." is corrupted.\n");}echo("\nScript error: the ".(($cli=(php_sapi_name()=='cli')) ?'ionCube':'<a href="https://www.ioncube.com">ionCube</a>')." Loader for PHP needs to be installed.\n\nThe ionCube Loader is the industry standard PHP extension for running protected PHP code,\nand can usually be added easily to a PHP installation.\n\nFor Loaders please visit".($cli?":\n\nhttps://get-loader.ioncube.com\n\nFor":' <a href="https://get-loader.ioncube.com">get-loader.ioncube.com</a> and for')." an instructional video please see".($cli?":\n\nhttp://ioncu.be/LV\n\n":' <a href="http://ioncu.be/LV">http://ioncu.be/LV</a> ')."\n\n");exit(199);
+include $_SERVER['DOCUMENT_ROOT'].'/init.php'; // 引入文件
+
+$jsonFile = $GLOBALS['ffmpeg-setting'];
+
+// 读取 JSON 文件内容
+function readJsonFile() {
+    global $jsonFile;
+    if (!file_exists($jsonFile)) {
+        return [];
+    }
+    $data = file_get_contents($jsonFile);
+    return json_decode($data, true);
+}
+
+// 查：根据 ID 查找项
+function getItem($id) {
+    $data = readJsonFile();
+    foreach ($data as $item) {
+        if ($item['id'] == $id) {
+            return $item;
+        }
+    }
+    return null;
+}
+
+
+
+
 ?>
-HR+cP/zXe0xVUuZp408Mc/mBp07HBKRQqtPclVAbvMtrBkVHzizr3QY6sr1OMxCVXZ318aCzJZJF
-lGLIriJKp8GShOBp8pStVp0aAEv1YNgAq7JZQS7WE5jeR8oEQxGwaWQDJCEIxLu1DsFBwKbv/s2Z
-C/aTg4lhHtVNGN5W/ctSB4tU/HGEfRRbr83i9oLsviJbKRMGyVVSmq2niXK+b7Kzwr/OwPL1dY/v
-wtz30rrS22FlLON3WsEz62Oj9/4PBo4Q/mB+BQGF01wniumQwMFQ7jY73s55SseWUjG0RBER6/ov
-EWxn34p/WepE88SBJY27sOvF/2ptgw1+fy3KGSI+h1wUcnMs6wC+wGn7uNUqhGtj6Lvfcq3yTOpu
-oof9OjbCQgSh9eX0HbXuQ8ALpo498wnw9jHgpCnxCfOXP1atZN1Pt0Xw78NAEKmfikO0Ht9XhGlv
-QXVSWjQELkCsokYPn+Uw3pZDciLGfGr4oGr94cxLHPdHyyYQ6kyQdv2mdUr49mp1Mj8OeBrJiflC
-RTGtR/Nw29hvKJ16j8EBbFMMXonY8IwAQFHbHTtgMbugOUoG+Kosf6LDzqMnoNApuyQZxl+5GJ4Z
-m+MHuLX6MmTC7c+QjNzAPR8DniBiEDjO/N+N2CsCSdteG/nqcWsj3cO0rZzNKjSRVfQyqc7C2ctj
-iaUii66q072BZeVkevDWticO0/F6kJbnknATyIQVgEC1BJgsR9FawLcu+cs2ZqOU7TF30+bxSPZi
-D6oxIgS0wNdnRPKHk1WmtgadRGOxuJgh/0OkfKu/cZYhn9TFWqoq9tnsC2O1c+NtJXZ+WJk6Fm/o
-pp7VppAdg9K5uj+lizCx9v0aUBOJi0Bdhq03rVrzTjlhYbCuATXbMEcFvH0WkvsEKOkpEgDhuoIb
-gDla3J7wzCMhUhnaMgNxQDUmljk7XfasShIG6cTnlmquV1hYWu/PcDMR//8YZ773uBU1XDZ2UnPU
-Pw+3T4C2P9O6pd7MynWXEz2sbkyOeTennvOvWuVWph7QI/hh5qC6/g2oZU4S0HG3kQ3YwEbw8Tlb
-LYviCuBmnyYsP0nC5g8ouzh7nKhdu4iQnx44jT3RqikV6rsjXrTYDfivzZdI+13m21t66ibnHYSO
-oAdRO6OP873OdKJ3GKTWQ+n6X6Gk8Ro2nAt9yS4mUW4ngjiuyA+uq0WCMDsGw/RXimV2U/VAffht
-Mbr63b0FR1Bnv+9aRO1MbQTZMHW86s+ZHWnz/OOGdL6/xa8gBk7JzqMAHgtYXYT5C5FQtVEFu9a6
-BTecJLH0GxBW32UsKfYvZfIkXRC9r3H2Px3V6wKWfoEEh4n6IB7BWpSxcmLBGH/OJ03ko/VevJ+g
-i6A19gCCB4HtjUekcgyc5/WMCy606U9kTE1HiVhmh92MfbdxE5ZHkb+5FiIN8dMWAWVt9uAzTVBI
-VYKnVW64bs62lz+wmgyDzoprwCBBxwo5SQ2HoDI8blToZYf3DYLRIGAdbXwP0BdNO+DhHPW/pXi/
-gqYI6t0MKGVDPGZYKazCBEtUQUKRPkxM3weQ6swmFdHgqMBjEgql0zCM0Sc49lFa1XhGCKAyQ1EY
-AhrrPS+s/kQbCNLR3LF131E81dN0wT0Imec8L0SumTbgvhb+evNaSYAhQyQu1/2Di0qw1rnB1Fhb
-Pe3qO/0n+4Nd19ECOalTr5ZaGF+bTWndZg6j9U09yZI9IW5qMhbCqYf8nbMQodJpBzWz4XgbmwOS
-V/0HfpiP6mwKViHQ/R+aZG7sVPnIqQ+bmsvZWC95WS3NriDKWIg5K0DI/zgZyL4aw/MFp05CJ0Oc
-m9xtvTXeImqiRucTlWsZIF9K+RSzQjunQ0fspvUjFhqWLE8i7jHhpDVn7UwTaiIoBgP/a6Ec/CxS
-+u0A5KCPILh2iOTTaoNTnm8Jh1sodDho9B2DIU6Ni1BWuZNoRBqV1Ad5mCMdrP8MdJSqmH3whTLi
-rJfCQULlwi/7yaaaH+Y+FfNcICqP64EPKB3VIAvrG1VxXljvUzJxeC/DkUrUuECMcti5NWCjlPnM
-PzDr564u1rrKiW6WfwjUD2hDa/lNAeST3eBfiO6FlC61LsDYyg1yEpqL57M/ysjA4vbGJ6G694qE
-Hv7/UGGrzJF9EENyj4iZ8KlzuR+DQGMBT/ftc/PfOtCiBPLumra/h/BDkXBkvHjn66ENWOF85wwh
-YioB/PMGT2vsp+JBHHTLewTskTGlO0GlTF4P+PVnzA/8fttZkdS=
